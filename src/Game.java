d914eaa8202144856306528c83b40e66f4950401
BT_1N3_17 <sample@bths.edu>
2024-03-22 11:03:38 -0400

    boardx

--- src/Game.java
+++ src/Game.java
@@ -2,8 +2,8 @@ import javax.swing.JPanel;
 import java.awt.*;
 
 public class Game extends JPanel{
-    public static final int width = 1100;
-    public static final int height = 800;
+    public static final int width = 900;
+    public static final int height = 900;
 
     public Game(){
         setPreferredSize(new Dimension(width, height));
@@ -14,7 +14,29 @@ public class Game extends JPanel{
 
     }
     public void paint(Graphics x){
-        super.paintComponent(x);
+       x.setColor(Color.BLACK);
+       x.fillRect(0,0,900,50);
+       x.fillRect(0,850,900,50);
+       x.fillRect(0,0,50,900);
+       x.fillRect(850,0,50,900);
+       int c = 0;
+       for(int row = 0; row < 8; row++){
+           for(int col = 0; col < 8; col++){
+               if(c == 0){
+                   x.setColor(Color.WHITE);
+                   c = 1;
+               } else {
+                   x.setColor(new Color(86, 188, 71));
+                   c = 0;
+               }
+               x.fillRect(row * 100 + 50, col * 100 + 50, 100, 100);
+           }
+           if(c == 0){
+               c = 1;
+           } else {
+               c = 0;
+           }
+       }
     }
 
 }
---
+++ src/King.java
@@ -0,0 +1,6 @@
+public class King extends Piece {
+    public King(String pos, String col) {
+        super(pos, col);
+    }
+
+}
