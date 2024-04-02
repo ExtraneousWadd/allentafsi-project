bf5fecc1c35b5363fd709f02c9632509d5c4033c
BT_1N3_16 <zdfsg@ddjsfj.dvkmfdkg>
2024-04-02 10:04:20 -0400

    Improved letter and number system (border)

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class Game extends JPanel{
     public static final int width = 900;
@@ -36,28 +37,40 @@ public class Game extends JPanel{
                c = 0;
            }
        }
-       int y = 800;
+       int ycoord = 800;
        int xcoord = 100;
        x.setFont(new Font("Arial", Font.PLAIN, 18));
-       for (int i = 0; i < 8; i++){
-           x.drawString(Integer.toString(i), 25, y);
-           y -= 100;
+       for (int i = 1; i < 9; i++){
+           x.drawString(Integer.toString(i), 25, ycoord);
+           ycoord -= 100;
        }
-       x.drawString("A", xcoord, 25);
-       xcoord += 100;
-       x.drawString("B", xcoord, 25);
-       xcoord += 100;
-       x.drawString("C", xcoord, 25);
-       xcoord += 100;
-       x.drawString("D", xcoord, 25);
-       xcoord += 100;
-       x.drawString("E", xcoord, 25);
-       xcoord += 100;
-       x.drawString("F", xcoord, 25);
-       xcoord += 100;
-       x.drawString("G", xcoord, 25);
-       xcoord += 100;
-       x.drawString("H", xcoord, 25);
+       ycoord = 800;
+       for (int i = 1; i < 9; i++){
+           x.drawString(Integer.toString(i), 860, ycoord);
+           ycoord -= 100;
+       }
+
+       ArrayList<String> letters = new ArrayList<>();
+       letters.add("A");
+       letters.add("B");
+       letters.add("C");
+       letters.add("D");
+       letters.add("E");
+       letters.add("F");
+       letters.add("G");
+       letters.add("H");
+
+       for (int i = 0; i < letters.size(); i++){
+           x.drawString(letters.get(i), xcoord, 25);
+           xcoord += 100;
+       }
+
+       xcoord = 100;
+       for (int i = 0; i < letters.size(); i++){
+           x.drawString(letters.get(i), xcoord, 875);
+           xcoord += 100;
+       }
+
 
     }
 
