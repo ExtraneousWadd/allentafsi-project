3e012d314662437902962593af32edc5f59c3b55
Tafsi Bhuiyan <tafsi.bhuiyan07@gmail.com>
2024-04-05 17:43:44 -0400

    Brought in black and white pngs but need to fix the transparency

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class Game extends JPanel {
     private Board board;
+    private Image[][] pieceImages;
+    private Image[][] whitePieceImages;
+    private Image[][] blackPieceImages;
+
     public static final int width = 900;
     public static final int height = 900;
 
@@ -11,6 +15,35 @@ public class Game extends JPanel {
         this.board = board;
         setPreferredSize(new Dimension(width, height));
         setBackground(Color.BLACK);
+        loadPieceImages();
+    }
+
+    private void loadPieceImages() {
+        pieceImages = new Image[8][8];
+        whitePieceImages = new Image[8][8];
+        blackPieceImages = new Image[8][8];
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Piece piece = board.getPiece(row, col);
+                if (piece != null) {
+                    String colorPrefix = piece.getColor().equals("white") ? "white_" : "black_";
+                    String pieceType = piece.getClass().getSimpleName().toLowerCase();
+                    String imagePath = "src/" + colorPrefix + pieceType + ".png";
+                    try {
+                        ImageIcon imageIcon = new ImageIcon(imagePath);
+                        Image image = imageIcon.getImage();
+                        pieceImages[row][col] = image;
+                        if (piece.getColor().equals("white")) {
+                            whitePieceImages[row][col] = image;
+                        } else {
+                            blackPieceImages[row][col] = image;
+                        }
+                    } catch (Exception e) {
+                        System.err.println("Error loading image: " + e.getMessage());
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -21,13 +54,17 @@ public class Game extends JPanel {
         drawChessboard(g);
 
         // Draw the pieces
-        drawPieces(g);
+        drawWhitePieces(g);
+        drawBlackPieces(g);
 
         // Draw coordinates
         drawCoordinates(g);
     }
 
     private void drawChessboard(Graphics g) {
+        int squareSize = 120;
+        int boardSize = squareSize * 8;
+
         int c = 0;
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
@@ -48,16 +85,24 @@ public class Game extends JPanel {
         }
     }
 
-    private void drawPieces(Graphics g) {
+    private void drawWhitePieces(Graphics g) {
+        if (whitePieceImages != null) {
+            drawPieces(g, whitePieceImages);
+        }
+    }
+
+    private void drawBlackPieces(Graphics g) {
+        if (blackPieceImages != null) {
+            drawPieces(g, blackPieceImages);
+        }
+    }
+
+    private void drawPieces(Graphics g, Image[][] pieceImages) {
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
-                Piece piece = board.getPiece(row, col);
-                if (piece != null) {
-                    String pieceType = piece.getClass().getSimpleName();
-                    String imagePath = "src/" + pieceType.toLowerCase() + "_piece.png";
-                    ImageIcon imageIcon = new ImageIcon(imagePath);
-                    Image image = imageIcon.getImage();
-                    g.drawImage(image, col * 100 + 50, row * 100 + 50, 100, 100, null);
+                Image image = pieceImages[row][col];
+                if (image != null) {
+                    g.drawImage(image, col * 100 + 50, row * 100 + 50, 100, 100, this);
                 }
             }
         }
@@ -104,4 +149,3 @@ public class Game extends JPanel {
         return new Dimension(width, height);
     }
 }
-
